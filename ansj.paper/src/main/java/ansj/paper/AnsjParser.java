899ab1037d3b9a096f941356d4808c8a4ee6595e
xuxinyi5354339 <xuxinyi@genedock.com>
2017-03-22 16:19:18 +0800

    add library

--- ansj.paper/src/main/java/ansj/paper/AnsjParser.java
+++ ansj.paper/src/main/java/ansj/paper/AnsjParser.java
@@ -11,24 +11,32 @@ import org.ansj.dic.LearnTool;
 import org.ansj.domain.Result;
 import org.ansj.domain.Term;
 import org.ansj.splitWord.analysis.NlpAnalysis;
+import org.ansj.splitWord.analysis.ToAnalysis;
 import org.bson.Document;
+
+import com.spreada.utils.chinese.ZHConverter;;
 
 public class AnsjParser {
     private DBClient client;
     private LearnTool learnTool;
     private NlpAnalysis nlpAnalysis;
+    private ToAnalysis toAnalysis;
+    private ZHConverter zhconverter;
 
     AnsjParser(String hostName, int port, String dbName, String colName) {
         client = new DBClient(hostName, port, dbName, colName);
         learnTool = new LearnTool();
         nlpAnalysis = new NlpAnalysis();
+        toAnalysis = new ToAnalysis();
         nlpAnalysis.setLearnTool(learnTool);
+        zhconverter = ZHConverter.getInstance(ZHConverter.SIMPLIFIED);
     }
 
-    void parse() throws IOException {
+    void nlpparse() throws IOException {
         List<Document> docs = client.find();
         for (Document doc : docs) {
-            String text = (String) doc.get("text");
+            String text = zhconverter.convert((String) doc.get("text"));
+            doc.put("text", text);
             Result words = nlpAnalysis.parseStr(text);
             ArrayList<Object> newWords = new ArrayList<Object>();
             for (Term word : words.getTerms()) {
@@ -49,9 +57,30 @@ public class AnsjParser {
         fout.close();
     }
 
+    void toparse() throws IOException {
+        List<Document> docs = client.find();
+        int num = 0;
+        for (Document doc : docs) {
+            num += 1;
+            System.out.println(num);
+            String text = (String) doc.get("text");
+            Result words = toAnalysis.parseStr(text);
+            ArrayList<Object> newWords = new ArrayList<Object>();
+            for (Term word : words.getTerms()) {
+                if (word.getName() != "null") {
+                    List<String> ele = Arrays.asList(word.getName(), word.getNatureStr());
+                    if (ele != null && ele.get(0).trim().length() > 0 && ele.get(0) != "null")
+                        newWords.add(ele);
+                }
+            }
+            doc.put("words", newWords);
+            client.update(doc);
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         AnsjParser parser = new AnsjParser("localhost", 27017, "paper", "hotel");
-        parser.parse();
+        parser.toparse();
     }
 
 }
