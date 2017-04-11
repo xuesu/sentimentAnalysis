0a4c275a0b924a7badcba0ac2d333aa020d28b12
xuesu <5354339xy@gmail.com>
2017-04-11 18:10:25 +0800

    changemiddir, alter network

--- Predictor.py
+++ Predictor.py
@@ -39,36 +39,30 @@ class Predictor:
             self.embed_with_natures_reshaped = tf.reshape(self.embed_with_natures, [self.batch_size, Mes.DG_SENTENCE_SZ, self.embed_out_sz])
             self.conv1 = tf.layers.conv1d(self.embed_with_natures_reshaped, Mes.PRE_CONV_L1_OUT_D,
                                          Mes.PRE_CONV1_L1_KERNEL_NUM,
-                                         Mes.PRE_CONV1_L1_STRIDE, name="Convnet1")
+                                         Mes.PRE_CONV1_L1_STRIDE, name="Convnet1", padding='same')
             self.conv2 = tf.layers.conv1d(self.embed_with_natures_reshaped, Mes.PRE_CONV_L1_OUT_D,
                                          Mes.PRE_CONV2_L1_KERNEL_NUM,
-                                         Mes.PRE_CONV2_L1_STRIDE, name="Convnet2")
+                                         Mes.PRE_CONV2_L1_STRIDE, name="Convnet2", padding='same')
             self.conv3 = tf.layers.conv1d(self.embed_with_natures_reshaped, Mes.PRE_CONV_L1_OUT_D,
                                          Mes.PRE_CONV3_L1_KERNEL_NUM,
-                                         Mes.PRE_CONV3_L1_STRIDE, name="Convnet3")
-            # self.conv4 = tf.layers.conv1d(self.embed_with_natures_reshaped, Mes.PRE_CONV_L1_OUT_D,
-            #                              Mes.PRE_CONV4_L1_KERNEL_NUM,
-            #                              Mes.PRE_CONV4_L1_STRIDE, name="Convnet4")
-            self.pool1 = tf.layers.max_pooling1d(self.conv1, Mes.PRE_POOL1_L1_POOL_SZ, Mes.PRE_POOL1_L1_STRIDE)
-            self.pool2 = tf.layers.max_pooling1d(self.conv2, Mes.PRE_POOL2_L1_POOL_SZ, Mes.PRE_POOL2_L1_STRIDE)
-            self.pool3 = tf.layers.max_pooling1d(self.conv3, Mes.PRE_POOL3_L1_POOL_SZ, Mes.PRE_POOL3_L1_STRIDE)
-            # self.pool4 = tf.layers.max_pooling1d(self.conv4, Mes.PRE_POOL4_L1_POOL_SZ, Mes.PRE_POOL4_L1_STRIDE)
-            # self.concat = tf.concat([self.conv1, self.conv2, self.conv3, self.conv4], 1)
-            # concat = tf.concat([self.pool1, self.pool2, self.pool3, self.pool4], 1)
-            concat = tf.concat([self.pool1, self.pool2, self.pool3], 1)
-            self.conv1_l2 = tf.layers.conv1d(concat, Mes.PRE_CONV_L2_OUT_D,
+                                         Mes.PRE_CONV3_L1_STRIDE, name="Convnet3", padding='same')
+            self.concat = tf.concat([self.conv1, self.conv2, self.conv3], 2)
+            self.conv1_l2 = tf.layers.conv1d(self.concat, Mes.PRE_CONV_L2_OUT_D,
                                          Mes.PRE_CONV1_L2_KERNEL_NUM,
                                          Mes.PRE_CONV1_L2_STRIDE, name="Convnet1_l2")
-            self.conv2_l2 = tf.layers.conv1d(concat, Mes.PRE_CONV_L2_OUT_D,
+            self.conv2_l2 = tf.layers.conv1d(self.concat, Mes.PRE_CONV_L2_OUT_D,
                                          Mes.PRE_CONV2_L2_KERNEL_NUM,
                                          Mes.PRE_CONV2_L2_STRIDE, name="Convnet2_l2")
-            self.conv3_l2 = tf.layers.conv1d(concat, Mes.PRE_CONV_L2_OUT_D,
+            self.conv3_l2 = tf.layers.conv1d(self.concat, Mes.PRE_CONV_L2_OUT_D,
                                          Mes.PRE_CONV3_L2_KERNEL_NUM,
                                          Mes.PRE_CONV3_L2_STRIDE, name="Convnet3_l2")
-            concat_l2 = tf.concat([self.conv1_l2, self.conv2_l2, self.conv3_l2], 1)
-            shape = concat_l2.get_shape().as_list()
+            self.pool1_l3 = tf.layers.max_pooling1d(self.conv1_l2, Mes.PRE_POOL1_L3_SIZE, Mes.PRE_POOL1_L3_STRIDE)
+            self.pool2_l3 = tf.layers.max_pooling1d(self.conv2_l2, Mes.PRE_POOL2_L3_SIZE, Mes.PRE_POOL2_L3_STRIDE)
+            self.pool3_l3 = tf.layers.max_pooling1d(self.conv3_l2, Mes.PRE_POOL3_L3_SIZE, Mes.PRE_POOL3_L3_STRIDE)
+            self.concat_l3 = tf.concat([self.pool1_l3, self.pool2_l3, self.pool3_l3], 1)
+            shape = self.concat_l3.get_shape().as_list()
             out_num = shape[1] * shape[2]
-            self.reshaped = tf.reshape(concat_l2, [-1, out_num])
+            self.reshaped = tf.reshape(self.concat_l3, [-1, out_num])
             self.lstm = tf.contrib.rnn.MultiRNNCell([
                 tf.contrib.rnn.BasicLSTMCell(
                     Mes.PRE_LSTM_SZ) for _ in range(Mes.PRE_LSTM_LAYER_NUM)])
@@ -159,21 +153,20 @@ class Predictor:
                     print "Average Loss at Step %d: %.10f" % (i, average_loss / Mes.PRE_VALID_TIME)
                     print "Average Train Accuracy %.2f%%" % (average_train_accuracy)
                     print "Validate Accuracy %.2f%%" % accuracy
-                    if accuracy > 70:
+                    if accuracy >= 80:
                         test_accuracy = self.test(session)
                         print "Test Accuracy %.2f%%" % test_accuracy
-                        self.saver.save(session, "%s/%.0f_%s.model" % (Mes.MODEL_SAVE_PATH, accuracy,
-                                                                       datetime.datetime.now().strftime("%y%m%d%H%M%S")))
-                        shutil.copy("Mes.py", "%s/%.0f_%s.Mes.py" % (Mes.MODEL_SAVE_PATH, accuracy,
-                                                                     datetime.datetime.now().strftime("%y%m%d%H%M%S")))
-                        shutil.copy("Predictor.py", "%s/%.0f_%s.Predictor.py" % (Mes.MODEL_SAVE_PATH,
-                                                                                 accuracy, datetime.datetime.now().
-                                                                                 strftime("%y%m%d%H%M%S")))
-
+                        if test_accuracy >= 80 and average_train_accuracy >= 80:
+                            mid_dir = "%s/%.0f_%.0f_%.0f_%s.model" % (Mes.MODEL_SAVE_PATH, average_train_accuracy,
+                                                                      accuracy, test_accuracy,
+                                                                      datetime.datetime.now().strftime("%y%m%d%H%M%S"))
+                            self.saver.save(session, mid_dir + "/model")
+                            shutil.copy("Mes.py", mid_dir + "/Mes.py")
+                            shutil.copy("Predictor.py", mid_dir + "/Predictor.py")
                     average_train_accuracy = 0.0
                     average_loss = 0.0
             accuracy = self.test(session)
-            print "Test Accuracy %.2f%%" % accuracy
+            print "Final Test Accuracy %.2f%%" % accuracy
 
 if __name__ == '__main__':
     mobile = pymongo.MongoClient("localhost", 27017).paper.mobile
