ce1e198b9addb23077aa5a801c79e13e92fa5a6f
xuesu <5354339xy@gmail.com>
2017-04-17 12:41:31 +0800

    data/collection

--- Mes.py
+++ Mes.py
@@ -1,5 +1,7 @@
 import os
-DATADIR = 'data'
+TRAIN_COL = "hotel_balanced"
+DATADIR = os.path.join("data", TRAIN_COL)
+os.mkdir(DATADIR)
 W2V_IMG_PATH = os.path.join(DATADIR, 'w2v.png')
 W2V_WORDS_PATH = os.path.join(DATADIR, 'w2v_words.json')
 W2V_WORDS_ID_PATH = os.path.join(DATADIR, 'w2v_words_id.json')
@@ -13,13 +15,12 @@ W2V_STEP_NUM = 10001
 W2V_VOC_LIMIT = 10000
 W2V_RARE_WORD = u'RAREWORD'
 
-TRAIN_COL = "mobile"
 
 DG_DIVIDE_FOLD = True
 DG_BATCH_SZ = 300
 DG_RNUM = 0
 DG_TEST_BATCH_SZ = 1
-DG_SENTENCE_SZ = 250 if TRAIN_COL == "hotel" else 100
+DG_SENTENCE_SZ = 250 if TRAIN_COL.startswith("hotel") else 100
 DG_FOLD_NUM = 11
 DG_FOLD_TEST_ID = 0
 DG_FOLD_VALID_ID = 1
@@ -36,8 +37,8 @@ PRE_LSTM_SZ = 128
 PRE_LINEAR1_SZ = 256
 PRE_LINEAR2_SZ = 8
 PRE_LINEAR3_SZ = LABEL_NUM
-PRE_E_LEARNING_RATE = 0.1
-PRE_E_LEARNING_RATE_NOLSTM = 0.001
+PRE_E_LEARNING_RATE = 0.01 if TRAIN_COL.startswith("hotel") else 0.1
+PRE_E_LEARNING_RATE_NOLSTM = 0.0001 if TRAIN_COL.startswith("hotel") else 0.001
 PRE_E_DECAY_STEP = 1000
 PRE_E_DECAY_RATE = 0.96
 PRE_G_CUT = 5
