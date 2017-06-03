603b3a750da0d5086cf3899061649655b30b9361
xuesu <1427934548@qq.com>
2017-06-03 09:31:15 +0800

    renew ratio

--- static/assets/js/communicate.js
+++ static/assets/js/communicate.js
@@ -4,11 +4,16 @@ var empty_prompt = "还什么都没有告诉我呢...";
 var empty_img_addr = "/static/images/empty.png";
 var rare_prompt = "我好像不太懂这些词的意思......";
 var rare_img_addr = "/static/images/rare.png";
-var good_prompt = "那么这次还算不错？我们会持续改进的！";
-var good_img_addr = "/static/images/good.png";
-var bad_prompt = "非常抱歉，下次不会再有这样的事情发生了。";
-var bad_img_addr = "/static/images/bad.png";
-var split_url = "/split_words/";
+var prompts = {
+    "-1": "非常抱歉，下次不会再有这样的事情发生了。",
+    "0": "那么这次还算不错？我们会持续改进的！",
+    "1": "谢谢光临，争取下次给您留下更好的印象"
+};
+var img_addrs = {
+    "-1":"/static/images/bad.png",
+    "0":"/static/images/good.png",
+    "1":"/static/images/bad.png"
+}
 var predict_url = "/predict/";
 
 
@@ -24,42 +29,37 @@ function pic_change(img_path){
 	$("#sys_photo").attr("src", img_path);
 }
 
-function show_result(logits){
-	if(logits[0] > logits[1]){
-		pic_change(bad_img_addr);
-		sys_say(bad_prompt);
-	}else{
-		pic_change(good_img_addr);
-		sys_say(good_prompt);
-	}
+function show_result(tag){
+	pic_change(img_addrs[tag.toString()]);
+	sys_say(prompts[tag.toString()]);
+	$("#loading").fadeOut(500);
 }
 
 function handle_predict(data){
-	rare_words = data.data.rare_words;
-	logits = data.data.logits;
+	words = data.data.words;
+	tag = data.data.tag;
+	var rare_words = []
+	for(var word in words){
+	    for(var info in word){
+	        if(info == "RareWord"){
+	            rare_words.push(word[0]);
+	            break;
+	        }
+	    }
+	}
 	if(rare_words.length == 0){
-		show_result(logits);
+		show_result(tag);
 	}else{
 		sys_say(rare_prompt);
 		pic_change(rare_img_addr);
 		sys_say(rare_words);
 		setTimeout("show_result(logits)", 500);
 	}
-	
+
 }
 
-function handle_split(data){
-	words = data.data.words;
-	prompt = "";
-	for (var i = 0;i < words.length;i += 1){
-		prompt += words[i][0] + ','
-	}
-	user_say(prompt);
-	data = {"words": JSON.stringify(words)};
-	$.post(predict_url,
-		data, handle_predict
-	);
-}
+model_type = "NOLSTM";
+col_name = "ctrip";
 
 function submit_d(){
 	data = $("#user_mes").val();
@@ -68,13 +68,26 @@ function submit_d(){
 		pic_change(empty_img_addr);
 	}else{
 		user_say(data);
+		$("#loading").fadeIn(500);
 		$("#user_mes").val("")
-		$.post(split_url, 
-			{"text": data}, handle_split
+		$.post(predict_url,
+			{"text": data, "model_type": model_type, "col_name": col_name},
+			handle_predict
 		);
 	}
 }
 
 $($("#sys_mes").val(welcome_prompt + "\n"));
 $($("#user_submit").click(submit_d));
-
+$($("#sec_en").click(function(){
+    col_name = "nlpcc_en";
+}));
+$($("#sec_zh").click(function(){
+    col_name = "ctrip";
+}));
+$($("#sec_cnnpl").click(function(){
+    model_type = "NOLSTM";
+}));
+$($("#sec_cnnlstmpl").click(function(){
+    model_type = "LSTM";
+}));
